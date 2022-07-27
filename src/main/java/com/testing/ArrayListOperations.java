665704c2ba56cbb4e3f4072a6316b1969d328746
Mayur505x <mayurpawar8691@gmail.com>
2022-07-27 08:52:07 +0530

    Added: Sorting ArrayList Program

--- src/main/java/com/testing/ArrayListOperations.java
+++ src/main/java/com/testing/ArrayListOperations.java
@@ -1,16 +1,14 @@
 package com.testing;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ArrayListOperations {
-    public static void main(String[] args) {
-        ArrayListOperations arrayListOperations = new ArrayListOperations();
-        arrayListOperations.removeElement(3);
-    }
 
-    public void removeElement(int elementIndex) {
-        List<Integer> list = new ArrayList<>();
+    static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) {
         list.add(55);
         list.add(66);
         list.add(89);
@@ -19,8 +17,17 @@ public class ArrayListOperations {
         list.add(11);
         list.add(9);
         System.out.println(list);
+        ArrayListOperations arrayListOperations = new ArrayListOperations();
+        arrayListOperations.sortArray();
+        arrayListOperations.removeElement(3);
+    }
 
+    public void sortArray() {
+        list.sort(Comparator.naturalOrder());
+        System.out.println("ArrayList sorted: " + list);
+    }
+    public void removeElement(int elementIndex) {
         list.remove(elementIndex - 1);
-        System.out.println(list);
+        System.out.println("3rd Element Removed: " + list);
     }
 }
